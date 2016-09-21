87f89e521e2f4672703b30370a537c7b70821543
news75 <news75@libero.it>
2016-09-21 22:33:51 +0200

    primo test funzionate

---
+++ src/test/java/receiptTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by giuseppe on 21/09/2016.
+ */
+public class receiptTest {
+
+    @Test
+    public void testProva()
+    {
+        assertEquals(1,1);
+    }
+
+}
