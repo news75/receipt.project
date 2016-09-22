12d798c497184a0ca609c9055a536599058e9287
news75 <news75@libero.it>
2016-09-22 14:40:38 +0200

    ho imparato a splittare una stringa

--- src/test/java/receiptTest.java
+++ src/test/java/receiptTest.java
@@ -8,11 +8,20 @@ import static org.junit.Assert.*;
 public class receiptTest {
 
     @Test
-    public void testParseGood()
-    {
+    public void testParseGood() {
         Item item = new Item("1 book at 12.49");
 
-        assertEquals(1,item.quantity());
+        assertEquals(1, item.quantity());
+    }
+
+    @Test
+    public void test_imparo_ad_usare_lo_split()
+    {
+        String stringa_da_parsere = "1 book at 12.49";
+
+        String[] tokens = stringa_da_parsere.split("[ ]+");
+
+        assertEquals("1", tokens[0]);
     }
 
 }
