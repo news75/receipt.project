b78a00ded51b8a43f15d9bc46bb6553b21262987
news75 <news75@libero.it>
2016-09-23 07:41:54 +0200

    refactor

--- src/main/java/ItemDetails.java
+++ src/main/java/ItemDetails.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 public class ItemDetails {
     private String itemDetails;
     private String descriptionToken;
+    private String quantityToken;
 
     public ItemDetails(String itemDetails) {
         this.itemDetails = itemDetails.trim();
@@ -13,7 +14,6 @@ public class ItemDetails {
     }
 
     public int quantity() {
-        String quantityToken = getTokens()[0];
         return Integer.parseInt(quantityToken);
     }
 
@@ -31,7 +31,7 @@ public class ItemDetails {
         String leftTokens = tokens[0];
         String rightTokens = tokens[1];
         String[] quantityDesctiptionTokens = leftTokens.split("[ ]",2);
-        String quantityToken = quantityDesctiptionTokens[0];
+        quantityToken = quantityDesctiptionTokens[0];
         descriptionToken = quantityDesctiptionTokens[1];
     }
 
