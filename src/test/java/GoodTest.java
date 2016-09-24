2f627c0fe9b089dbbfda12567c080e0d5de242b8
news75 <news75@libero.it>
2016-09-24 09:52:35 +0200

    refactor

--- src/test/java/GoodTest.java
+++ src/test/java/GoodTest.java
@@ -8,11 +8,13 @@ import static org.junit.Assert.*;
  * Created by giuseppe on 24/09/2016.
  */
 public class GoodTest {
-    
+
     @Test
     public void testRoundingRules(){
-        BigDecimal value = new BigDecimal("1.00");
-        assertEquals(new BigDecimal("1.00"), Good.roundingRules(value));
+
+        assertEquals(new BigDecimal("1.00"), Good.roundingRules(new BigDecimal("1.00")));
     }
+
+
 
 }
