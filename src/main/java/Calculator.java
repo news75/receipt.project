4448d721c05ec0d019f726d0ae249d228fba2efc
news75 <news75@libero.it>
2016-09-24 16:38:13 +0200

    refactor

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,10 +8,7 @@ public class Calculator {
     public BigDecimal evaluate(ItemDetails itemDetails, Tax tax) {
 
 
-        return getTaxAmmount();
-    }
-
-    public BigDecimal getTaxAmmount() {
         return new BigDecimal("0.00");
     }
+
 }
