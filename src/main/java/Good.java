6392cfe83143ca0298cab213d5deb77734ce4f8f
news75 <news75@libero.it>
2016-09-24 09:14:59 +0200

    refactor

--- src/main/java/Good.java
+++ src/main/java/Good.java
@@ -4,8 +4,15 @@ import java.math.BigDecimal;
  * Created by giuseppe on 24/09/2016.
  */
 public class Good {
+    private String goodDescription;
+    private ItemDetails itemDetails;
+    private Tax tax;
+
     public Good(String goodDescription, ItemDetails itemDetails, Tax tax) {
 
+        this.goodDescription = goodDescription;
+        this.itemDetails = itemDetails;
+        this.tax = tax;
     }
 
     public BigDecimal taxAmmount() {
