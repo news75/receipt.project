57975512815d8497b231ee677ab5522c9ad6dc74
news75 <news75@libero.it>
2016-09-25 09:31:08 +0200

    refactor

--- src/main/java/Good.java
+++ src/main/java/Good.java
@@ -5,15 +5,9 @@ import java.math.RoundingMode;
  * Created by giuseppe on 24/09/2016.
  */
 public class Good {
-    private String goodDescription;
-    private ItemDetails itemDetails;
-    private Tax tax;
 
     public Good(String goodDescription, ItemDetails itemDetails, Tax tax) {
 
-        this.goodDescription = goodDescription;
-        this.itemDetails = itemDetails;
-        this.tax = tax;
     }
 
     public static BigDecimal roundingRules(BigDecimal value) {
