7a76d75f759bd9e1f818fcaf7abcb6b102d5fb8b
news75 <news75@libero.it>
2016-09-25 23:46:14 +0200

    refactor

--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -8,14 +9,19 @@ import static org.junit.Assert.*;
  * Created by giuseppe on 25/09/2016.
  */
 public class BasketTest {
+    private Basket basket;
+
+    @Before
+    public void setUp(){
+        Order order1 = new FakeOrder(null,null).initWithTaxedPriceAndTax("10.00","1.00");
+        Order order2 = new FakeOrder(null,null).initWithTaxedPriceAndTax("20.00","2.00");
+        basket = new Basket();
+        basket.add(order1);
+        basket.add(order2);
+    }
 
     @Test
     public void testBasket(){
-        Order order1 = new FakeOrder(null,null).initWithTaxedPriceAndTax("10.00","1.00");
-        Order order2 = new FakeOrder(null,null).initWithTaxedPriceAndTax("20.00","2.00");
-        Basket basket = new Basket();
-        basket.add(order1);
-        basket.add(order2);
 
         assertEquals(new BigDecimal("3.00"), basket.salesTaxes());
         assertEquals(new BigDecimal("30.00"), basket.total());
@@ -23,11 +29,6 @@ public class BasketTest {
 
     @Test
     public void TestBasketPrintReceipt(){
-        Order order1 = new FakeOrder(null,null).initWithTaxedPriceAndTax("10.00","1.00");
-        Order order2 = new FakeOrder(null,null).initWithTaxedPriceAndTax("20.00","2.00");
-        Basket basket = new Basket();
-        basket.add(order1);
-        basket.add(order2);
 
         FakeReceipt receipt = new FakeReceipt();
 
