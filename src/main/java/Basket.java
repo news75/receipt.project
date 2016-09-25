fb2fa6fd1aa8266ee89db2f6376ba99fadf7a278
news75 <news75@libero.it>
2016-09-25 10:05:34 +0200

    implementazione fake per testare l'ammontare delle tasse di un basket

---
+++ src/main/java/Basket.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+/**
+ * Created by giuseppe on 25/09/2016.
+ */
+public class Basket {
+    public void add(String goodDescription) {
+
+    }
+
+    public BigDecimal salesTaxes() {
+        return new BigDecimal("1.50");
+    }
+}
